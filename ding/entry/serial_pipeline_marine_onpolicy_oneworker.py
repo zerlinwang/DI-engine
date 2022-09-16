9c250f469ae94d72aac297d384e61ab1c6df19fe
wangzilin <wangzilin@sensetime.com>
2022-09-16 10:06:11 +0800

    update entry

--- ding/entry/serial_pipeline_marine_onpolicy_oneworker.py
+++ ding/entry/serial_pipeline_marine_onpolicy_oneworker.py
@@ -15,7 +15,7 @@ from ding.reward_model import create_reward_model
 from ding.utils import set_pkg_seed
 
 
-def serial_pipeline_onpolicy(
+def serial_pipeline_marine_onpolicy_oneworker(
         input_cfg: Union[str, Tuple[dict, dict]],
         seed: int = 0,
         env_setting: Optional[List[Any]] = None,
@@ -49,6 +49,14 @@ def serial_pipeline_onpolicy(
     # Create main components: env, policy
     if env_setting is None:
         env_fn, collector_env_cfg, evaluator_env_cfg = get_vec_env_setting(cfg.env)
+        collector_env_cfg[0].map_name = "5m_vs_6m"
+        collector_env_cfg[1].map_name = "5m_vs_6m"
+        collector_env_cfg[2].map_name = "8m_vs_9m"
+        collector_env_cfg[3].map_name = "8m_vs_9m"
+        evaluator_env_cfg[0].map_name = "5m_vs_6m"
+        evaluator_env_cfg[1].map_name = "5m_vs_6m"
+        evaluator_env_cfg[2].map_name = "8m_vs_9m"
+        evaluator_env_cfg[3].map_name = "8m_vs_9m"
     else:
         env_fn, collector_env_cfg, evaluator_env_cfg = env_setting
     collector_env = create_env_manager(cfg.env.manager, [partial(env_fn, cfg=c) for c in collector_env_cfg])
